4408f5a0b2a0b0a72ab0a8c66246732b222e37e2
nwekeKent <Nwekep14@gmail.com>
2020-08-25 11:10:50 +1300

    changed the title and added an id to the button

---
+++ script/script.js
@@ -0,0 +1,19 @@
+var button = $('#btn');
+var body = $('body');
+var colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'teal', 'biege', 'lightblue', 'black']
+
+body.css({
+    backgroundColor: 'teal'
+})
+button.click(changeBackground)
+
+function changeBackground() {
+    var colorIndex = parseInt(Math.random() * colors.length)
+    body.css({
+        backgroundColor: colors[colorIndex]
+    })
+
+    button.css({
+        backgroundColor: colors[colorIndex]
+    })
+}
